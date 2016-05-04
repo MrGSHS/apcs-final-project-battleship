4698e00526939bda4a2f75189209f06e69aa620a
Alex Pieprzycki <apieprz6@students.d125.org>
2016-05-04 14:35:39 -0500

    Work on the move() method in Battleship

--- Battleship/src/battleship/Battleship.java
+++ Battleship/src/battleship/Battleship.java
@@ -16,20 +16,43 @@ public class Battleship {
     private int row;
     private int col;
     private int[] conditionOfShip;
+    private boolean north= false;
+    private boolean east = false;
+    private boolean south = false;
+    private boolean west = false;
+    private boolean cw = false;
+    private boolean ccw = false;
     /**
      * @param args the command line arguments
      */
-    public Battleship(int size, int speed, Pattern pattern, int row, int col){
+    public Battleship(int size, int speed, Pattern pattern, int row, int col, String orientation, String direction){
         this.size = size;
         this.speed = speed;
         this.pattern = pattern;
         this.row = row;
         this.col = col;
         conditionOfShip = new int[size];
+        if(orientation.equals("north"))
+            north = true;
+        else if(orientation.equals("east"))
+            east = true;
+        else if(orientation.equals("south"))
+            south = true;
+        else
+            west = true;
+        if(direction.equals("cw"))
+            cw = true;
+        else
+            ccw = true;
     }
     
     int[][] move(int[][] gameBoard){
-        
+        int[] tempArray = pattern.getPosition(speed, row, col);
+        row = tempArray[0];
+        col = tempArray[1];
+        if(tempArray[2]==0){
+            
+        }
     }
     
 }
