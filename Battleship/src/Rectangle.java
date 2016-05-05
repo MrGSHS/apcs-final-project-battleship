d2ca47fb10155d590a51b15f94012b70bf89efaa
Alex Pieprzycki <apieprz6@students.d125.org>
2016-05-05 15:24:17 -0500

    worked on getNewPosition() in Rectangle

--- Battleship/src/Rectangle.java
+++ Battleship/src/Rectangle.java
@@ -10,7 +10,7 @@
  */
 public class Rectangle extends Pattern {
     private final int boardSize=30;
-    private int width,length;
+    private int width,length,startR,startC;
     private int[][] patternOnBoard = new int[boardSize][boardSize];
     
     /**
@@ -23,7 +23,9 @@ public class Rectangle extends Pattern {
     public Rectangle(int startR, int startC, int width, int length){
         super();
         this.width=width;
-        this.length=length;
+        this.length=length; // HEIGHT U DUMBASS
+        this.startR = startR;
+        this.startC = startC;
         for(int r=0;r<=boardSize;r++){
             for(int c=0;c<=boardSize;c++){
                 if(r == startR  && c >= startC && c<=startC + width){
@@ -48,6 +50,51 @@ public class Rectangle extends Pattern {
     @Override
     public void getNewPosition(Battleship battleship){
         int speed = battleship.getSpeed();
+        String orientation = battleship.getOrientation();
+        boolean north = false;
+        boolean east = false;
+        boolean south = false;
+        boolean west = false;
+        if(orientation.equals("north"))
+            north = true;
+        else if(orientation.equals("east"))
+            east = true;
+        else if(orientation.equals("south"))
+            south = true;
+        else
+            west = true;
+        int intR = battleship.getRow();
+        int intC = battleship.getCol();
+        if(intR == startR){
+            if(intC - speed >= startC){
+                battleship.setCol(intC-speed);
+            }
+            else{
+                int down = startC - (intC - speed);
+                battleship.setRow(intR - down);
+                battleship.setCol(startC);
+            }
+        }
+        else if(intC == startC){
+            if(intR + speed <= startR + length){
+                battleship.setRow(intR+speed);
+            }
+            else{
+                int over = (intR + speed) - (startR + length);
+                battleship.setCol(intC + over);
+                battleship.setRow(intR + length);
+            }
+        }
+        else if(intR == startR+length){
+            if(intC+speed <= startC + width){
+                battleship.setCol(intC+speed);
+            }
+            else{
+                //START HERE, IF PAST BOTTOM RIGHT CORNER
+            }
+        }
+        
+        //ADD TOPRIGHT CORNER
         
     }
 }
