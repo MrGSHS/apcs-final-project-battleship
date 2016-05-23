3b85ef4355ffb3b88e9df3bd235949553f1b7150
Maor Gozalzani <maorgozalzani@Maors-MacBook-Air.local>
2016-05-23 15:26:05 -0500

    Finished set up except printing array. Change array to array list

--- Battleship/src/SetUp.java
+++ Battleship/src/SetUp.java
@@ -13,8 +13,10 @@ import java.awt.event.KeyEvent;
 public class SetUp extends javax.swing.JFrame {
 
     private String speed, size, row, col, patternW, patternL;
-    Rectangle r1, r2, r3, r4, r5, r6;
-    Battleship b1, b2, b3, b4, b5, b6;
+    private Rectangle r1, r2, r3, r4, r5, r6;
+    private Battleship b1, b2, b3, b4, b5, b6;
+    private Rectangle[] rArray = new Rectangle[6];
+    private Battleship[] bArray = new Battleship[6];
 
     /**
      * Creates new form SetUp
@@ -32,6 +34,7 @@ public class SetUp extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         SizeLabel = new javax.swing.JLabel();
         SpeedLabel = new javax.swing.JLabel();
@@ -48,48 +51,40 @@ public class SetUp extends javax.swing.JFrame {
         PLengthText = new javax.swing.JTextField();
         AddShipButton = new javax.swing.JButton();
         newShip = new javax.swing.JButton();
-        ErrorText = new javax.swing.JTextField();
-        jPanel2 = new javax.swing.JPanel();
+        errorLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 527, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 384, Short.MAX_VALUE)
+        );
 
         SizeLabel.setText("Enter Size:");
 
         SpeedLabel.setText("Enter Speed");
 
+        SpeedText.setColumns(3);
         SpeedText.setText("          ");
         SpeedText.setToolTipText("Enter Speed");
-        SpeedText.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                SpeedTextMouseClicked(evt);
-            }
-        });
         SpeedText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 SpeedTextActionPerformed(evt);
             }
         });
-        SpeedText.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                SpeedTextKeyPressed(evt);
-            }
-        });
 
+        SizeText.setColumns(3);
         SizeText.setText("          ");
         SizeText.setToolTipText("Enter Size");
-        SizeText.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                SizeTextMouseClicked(evt);
-            }
-        });
         SizeText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 SizeTextActionPerformed(evt);
-            }
-        });
-        SizeText.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                SizeTextKeyPressed(evt);
             }
         });
 
@@ -97,81 +92,45 @@ public class SetUp extends javax.swing.JFrame {
 
         bRow.setText("Enter Ship's Row:");
 
+        RowText.setColumns(3);
         RowText.setText("          ");
         RowText.setToolTipText("Enter Row");
-        RowText.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                RowTextMouseClicked(evt);
-            }
-        });
         RowText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 RowTextActionPerformed(evt);
             }
         });
-        RowText.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                RowTextKeyPressed(evt);
-            }
-        });
 
         bColLabel.setText("Enter Ship's Col:");
 
+        ColText.setColumns(3);
         ColText.setText("          ");
         ColText.setToolTipText("Enter Column");
-        ColText.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                ColTextMouseClicked(evt);
-            }
-        });
         ColText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 ColTextActionPerformed(evt);
             }
         });
-        ColText.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                ColTextKeyPressed(evt);
-            }
-        });
 
         PWidthLabel.setText("Enter Pattern's Width:");
 
+        PWidthText.setColumns(3);
         PWidthText.setText("          ");
         PWidthText.setToolTipText("Can't be smaller than size");
-        PWidthText.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                PWidthTextMouseClicked(evt);
-            }
-        });
         PWidthText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 PWidthTextActionPerformed(evt);
             }
         });
-        PWidthText.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                PWidthTextKeyPressed(evt);
-            }
-        });
 
         PLengthLabel.setText("Enter Pattern's length:");
 
+        PLengthText.setColumns(3);
         PLengthText.setText("          ");
         PLengthText.setToolTipText("Can't be smaller than size");
-        PLengthText.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                PLengthTextMouseClicked(evt);
-            }
-        });
         PLengthText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 PLengthTextActionPerformed(evt);
-            }
-        });
-        PLengthText.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyPressed(java.awt.event.KeyEvent evt) {
-                PLengthTextKeyPressed(evt);
             }
         });
 
@@ -190,9 +149,8 @@ public class SetUp extends javax.swing.JFrame {
             }
         });
 
-        ErrorText.setForeground(new java.awt.Color(250, 0, 0));
-        ErrorText.setText("          ");
-        ErrorText.setToolTipText("");
+        errorLabel.setForeground(new java.awt.Color(250, 0, 0));
+        errorLabel.setText("\"Please enter valid values      ");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -203,7 +161,7 @@ public class SetUp extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(PWidthLabel)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(PWidthText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addContainerGap())
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -231,19 +189,18 @@ public class SetUp extends javax.swing.JFrame {
                             .addComponent(ColText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(RowText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(27, 27, 27)
-                        .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(22, 22, 22)
-                        .addComponent(newShip)))
+                        .addComponent(newShip))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(errorLabel)
+                            .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(48, 48, 48)
                 .addComponent(AddShipButton)
                 .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(ErrorText))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -278,20 +235,9 @@ public class SetUp extends javax.swing.JFrame {
                 .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(AddShipButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(ErrorText, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(24, Short.MAX_VALUE))
-        );
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 336, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 381, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+                .addComponent(errorLabel)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -301,17 +247,17 @@ public class SetUp extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(37, 37, 37)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 299, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 296, Short.MAX_VALUE))
         );
 
         pack();
@@ -328,11 +274,19 @@ public class SetUp extends javax.swing.JFrame {
 
     private void AddShipButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddShipButtonActionPerformed
         // TODO add your handling code here:
-        if (b1.getSize() == 0) {
-            r1 = new Rectangle(Integer.parseInt(row), Integer.parseInt(col), Integer.parseInt(patternW), Integer.parseInt(patternL));
-            b1 = new Battleship(Integer.parseInt(size), Integer.parseInt(speed), r1, Integer.parseInt(row), Integer.parseInt(col));
+        int speed = Integer.parseInt(this.speed);
+        int size = Integer.parseInt(this.size);
+        int row = Integer.parseInt(this.row);
+        int col = Integer.parseInt(this.col);
+        int pW = Integer.parseInt(patternW);
+        int pL = Integer.parseInt(patternL);
+        for (int i = 0; i < bArray.length; i++) {
+            if (bArray[i] == null) {
+                rArray[i] = new Rectangle(row, col, pW, pL);
+                bArray[i] = new Battleship(size, speed, rArray[i], row, col);
+            }
         }
-        
+
 
     }//GEN-LAST:event_AddShipButtonActionPerformed
 
@@ -356,78 +310,6 @@ public class SetUp extends javax.swing.JFrame {
         patternL = PLengthText.getText();
     }//GEN-LAST:event_PLengthTextActionPerformed
 
-    private void SizeTextKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_SizeTextKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            size = this.SizeText.getText();
-        }
-    }//GEN-LAST:event_SizeTextKeyPressed
-
-    private void SizeTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SizeTextMouseClicked
-        // TODO add your handling code here:
-        SizeText.requestFocusInWindow();
-    }//GEN-LAST:event_SizeTextMouseClicked
-
-    private void SpeedTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_SpeedTextMouseClicked
-        // TODO add your handling code here:
-        SpeedText.requestFocusInWindow();
-    }//GEN-LAST:event_SpeedTextMouseClicked
-
-    private void SpeedTextKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_SpeedTextKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            speed = this.SpeedText.getText();
-        }
-    }//GEN-LAST:event_SpeedTextKeyPressed
-
-    private void RowTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RowTextMouseClicked
-        // TODO add your handling code here:
-        RowText.requestFocusInWindow();
-    }//GEN-LAST:event_RowTextMouseClicked
-
-    private void RowTextKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_RowTextKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            row = this.RowText.getText();
-        }
-    }//GEN-LAST:event_RowTextKeyPressed
-
-    private void ColTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ColTextMouseClicked
-        // TODO add your handling code here:
-        ColText.requestFocusInWindow();
-    }//GEN-LAST:event_ColTextMouseClicked
-
-    private void ColTextKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_ColTextKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            col = this.ColText.getText();
-        }
-    }//GEN-LAST:event_ColTextKeyPressed
-
-    private void PWidthTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PWidthTextMouseClicked
-        // TODO add your handling code here:
-        PWidthText.requestFocusInWindow();
-    }//GEN-LAST:event_PWidthTextMouseClicked
-
-    private void PWidthTextKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_PWidthTextKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            patternW = this.PWidthText.getText();
-        }
-    }//GEN-LAST:event_PWidthTextKeyPressed
-
-    private void PLengthTextMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_PLengthTextMouseClicked
-        // TODO add your handling code here:
-        PLengthText.requestFocusInWindow();
-    }//GEN-LAST:event_PLengthTextMouseClicked
-
-    private void PLengthTextKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_PLengthTextKeyPressed
-        // TODO add your handling code here:
-        if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
-            patternL = this.PLengthText.getText();
-        }
-    }//GEN-LAST:event_PLengthTextKeyPressed
-
     private void newShipActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newShipActionPerformed
         // TODO add your handling code here:
         SizeText.setText("");
@@ -436,9 +318,9 @@ public class SetUp extends javax.swing.JFrame {
         ColText.setText("");
         PWidthText.setText("");
         PLengthText.setText("");
-        ErrorText.setText("");
+        errorLabel.setVisible(false);
     }//GEN-LAST:event_newShipActionPerformed
-    
+
     /**
      * @param args the command line arguments
      */
@@ -477,7 +359,6 @@ public class SetUp extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AddShipButton;
     private javax.swing.JTextField ColText;
-    private javax.swing.JTextField ErrorText;
     private javax.swing.JLabel PLengthLabel;
     private javax.swing.JTextField PLengthText;
     private javax.swing.JLabel PWidthLabel;
@@ -489,6 +370,7 @@ public class SetUp extends javax.swing.JFrame {
     private javax.swing.JTextField SpeedText;
     private javax.swing.JLabel bColLabel;
     private javax.swing.JLabel bRow;
+    private javax.swing.JLabel errorLabel;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JButton newShip;
